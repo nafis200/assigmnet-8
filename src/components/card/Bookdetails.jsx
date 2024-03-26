d5af4d63f66d32662196b4aca70467c20f21f733
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-26 15:52:30 +0600

    toast add

--- src/components/card/Bookdetails.jsx
+++ src/components/card/Bookdetails.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
 import Booktags from "./Booktags";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
 const Bookdetails = ({book}) => {
     const {bookId,bookName,author,image,review,totalPages,rating,category,tags,publisher,yearOfPublishing} = book
+
+    const [read,setRead] = useState(true)
+    const handleread = (id)=>{
+      toast.success("Books Added to Read List", {
+        theme: "colored"
+      })
+      setRead(false);
+    }
+
+    const handleWish = ()=>{
+         if(read){
+          toast.success("Books Added to Wish List", {
+            theme: "colored"
+          })
+         }
+         else{
+          toast.error("You have already add this books",{
+            theme:"colored"
+            
+          })
+         }
+    }
+
+
     return (
         <div className="mt-20">
           
@@ -35,15 +62,15 @@ const Bookdetails = ({book}) => {
                </div>
               
               <div className=" flex gap-5">
-              <button className="btn p-4 text-center mt-8 w-[100px]">Read</button>
-          <button className="btn p-4 text-center bg-blue-500 mt-8 w-[100px] text-white">Wishlist</button>
+              <button onClick={()=>handleread(bookId)} className="btn p-4 text-center mt-8 w-[100px]">Read</button>
+          <button onClick={handleWish} className="btn p-4 text-center bg-blue-500 mt-8 w-[100px] text-white">Wishlist</button>
          
               </div>
 
            </div>
              
          </section>  
-
+          <ToastContainer></ToastContainer>
         </div>
     );
 };
