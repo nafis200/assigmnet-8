e193acd9a18dd178b198fa1d865b3d39522f0bf5
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-26 22:39:36 +0600

    error done

--- src/components/Llistedbook/ListedBooks.jsx
+++ src/components/Llistedbook/ListedBooks.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router";
+import { getBooks } from "../utility/Localstorage";
+
 const ListedBooks = () => {
+    const Loaders = useLoaderData()
+    const [data, setData] = useState([])
+    useEffect(()=>{
+         const stored = getBooks()
+         if(Loaders.length > 0){
+             const Books = [];
+             for(const id of stored){
+                const finds = Loaders.find(job => job.bookId === id)
+                if(finds){
+                    Books.push(finds);
+                }
+             }
+             setData(Books)
+         }
+    },[])
+    
     return (
         <div>
-            <h1 className="text-2xl">hellow listed books</h1>
+            <h1 className="text-2xl">hellow listed books:{data.length}</h1>
         </div>
     );
 };
