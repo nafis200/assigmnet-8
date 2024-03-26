d1ad53aea41db39227f6c0d093a07e1f61e18ef8
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-26 22:43:41 +0600

    error is fully done

--- src/components/Llistedbook/ListedBooks.jsx
+++ src/components/Llistedbook/ListedBooks.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router";
-import { getBooks } from "../utility/Localstorage";
+import { getBooks, getwish } from "../utility/Localstorage";
 
 const ListedBooks = () => {
     const Loaders = useLoaderData()
     const [data, setData] = useState([])
+    const[listdata,setListdata] = useState([])
     useEffect(()=>{
          const stored = getBooks()
          if(Loaders.length > 0){
@@ -18,10 +19,25 @@ const ListedBooks = () => {
              setData(Books)
          }
     },[])
+
+    useEffect(()=>{
+        const stored1 = getwish()
+         if(Loaders.length > 0){
+             const Books1 = [];
+             for(const id of stored1){
+                const finds1 = Loaders.find(job => job.bookId === id)
+                if(finds1){
+                    Books1.push(finds1);
+                }
+             }
+             setListdata(Books1)
+         }
+    },[])
     
     return (
         <div>
             <h1 className="text-2xl">hellow listed books:{data.length}</h1>
+            <h1 className="text-2xl">List data:{listdata.length}</h1>
         </div>
     );
 };
